a3868f4bc1dafc841092cf8b055ac7868f9352f9
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-12-20 01:41:20 +1300

    Get demo working, but unstyled

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {processLines} from './lineProcessor'
+
+class ReplArea extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      textIn: 'PLACE 0,0,NORTH\nMOVE\nREPORT\n'
+    }
+  }
+
+  getTextOut () {
+    const linesIn = this.state.textIn.split(/\r?\n/)
+    const linesOut = processLines(linesIn)[1]
+    const textOut = linesOut.join('\n')
+    return textOut
+  }
+
+  handleTextIn (event) {
+    this.setState({textIn: event.target.value})
+  }
+
+  render () {
+    return (
+      <div>
+        <textarea value={this.state.textIn} onChange={this.handleTextIn.bind(this)} />
+        <textarea value={this.getTextOut()} readOnly />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <ReplArea />,
   document.getElementById('root')
 )
