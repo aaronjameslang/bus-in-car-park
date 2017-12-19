8d78726a4d8d58fdae5190f1338cbe4a4a5b2dbc
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-12-20 00:42:55 +1300

    Hide bundles from linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: {
-    'mocha.js': './node_modules/mocha/mocha.js',
-    'tests.js': [
+    'mocha.min.js': './node_modules/mocha/mocha.js',
+    'tests.min.js': [
       './test/Pose.js', // TODO this should be a pattern match
       './test/engine.js',
       './test/lineProcessor.js'
