42d88a1822c418b892b44930379237fc87aedddc
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-12-20 17:27:33 +1300

    Add comments to code

--- src/engine.js
+++ src/engine.js
@@ -1,7 +1,10 @@
+/** @module */
+
 const R = require('ramda')
 
 const Pose = require('./Pose')
 
+/** @see module:engine.move */
 function move (pose) {
   return pose && Pose(
     pose.x + (2 - pose.f) % 2,
@@ -11,6 +14,15 @@ function move (pose) {
   )
 }
 
+/**
+ * Calculate a turn
+ *
+ * If the initial pose is unset, the result will be also
+ *
+ * @param {int} Δf - The change in f, how much to turn
+ * @param {module:Pose.Pose} pose - The pose before turning
+ * @returns {module:Pose.Pose} The pose after turning
+ */
 function turn (Δf, pose) {
   return pose && Pose(
     pose.x,
@@ -21,9 +33,55 @@ function turn (Δf, pose) {
 }
 
 module.exports = {
+  /**
+   * Calculate the pose after a place
+   *
+   * This function is curried
+   *
+   * @function
+   * @param {int} x - Horizontal position, between 0-4 inclusive, 0 is south
+   * @param {int} y - Vertical   position, between 0-4 inclusive, 0 is west
+   * @param {int} f - Facing/orientation , between 0-3 inclusive
+   * @param {module:Pose.Pose} pose - The pose before placing
+   * @returns {module:Pose.Pose} The pose after
+   */
   place: R.curry(Pose),
+  /**
+   * Calculate the pose after a move
+   *
+   * @function
+   * @param {module:Pose.Pose} pose - The pose before moving
+   * @returns {module:Pose.Pose} The pose after
+   */
   move: move,
+  /**
+   * Calculate the pose after a left turn
+   *
+   * @see module:engine~turn
+   * @see module:engine.right
+   *
+   * @function
+   * @param {module:Pose.Pose} pose - The pose before turning
+   * @returns {module:Pose.Pose} The pose after turning
+   */
   left: R.partial(turn, [-1]),
+  /**
+   * Calculate the pose after a right turn
+   *
+   * @see module:engine~turn
+   * @see module:engine.left
+   *
+   * @function
+   * @param {module:Pose.Pose} pose - The pose before turning
+   * @returns {module:Pose.Pose} The pose after turning
+   */
   right: R.partial(turn, [+1]),
+  /**
+   * Calculate the pose after a report, this is a no-op
+   *
+   * @function
+   * @param {module:Pose.Pose} pose - The pose before
+   * @returns {module:Pose.Pose} The pose after
+   */
   report: R.identity
 }
